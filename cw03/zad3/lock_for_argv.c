a804231f2c5c428a85416facc0587bcff690782f
c-i-a-s-t-e-k <jezowdominik@gmail.com>
2023-03-23 18:32:19 +0000

    cw03 zad3 done

---
+++ cw03/zad3/lock_for_argv.c
@@ -0,0 +1,5 @@
+//
+// Created by ciastek on 23.03.23.
+//
+
+#include "lock_for_argv.h"
---
+++ cw03/zad3/lock_for_argv.h
@@ -0,0 +1,8 @@
+//
+// Created by ciastek on 23.03.23.
+//
+
+#ifndef SYSOP_LOCK_FOR_ARGV_H
+#define SYSOP_LOCK_FOR_ARGV_H
+
+#endif //SYSOP_LOCK_FOR_ARGV_H
